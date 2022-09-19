bc0aa9988c11abb8777f35ab5a6ab3a1d7295985
Nachocheeze18 <5581@holbertonstudents.com>
2022-09-19 08:38:14 -0700

    script that prints with propper grammar, but the outcome is a piece of art,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/*
+ * File: 5-printf.c
+ */
+int main(void)
 
-int present=1;
-if (present)
-{printf("with proper grammar, but outcome is a piece of art,\n");
-	return 0;
+{
+	printf("with proper grammar, but outcome is a piece of art,\n");
+
+	return (0);
 }
