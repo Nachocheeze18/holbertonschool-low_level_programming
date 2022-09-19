6e29dee9caa4dd37aef574680963123b54293124
Nachocheeze18 <5581@holbertonstudents.com>
2022-09-19 09:27:01 -0700

    script to print the size of various types on the computer it is running on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 /*
- * produce the same output as in the example
+ * File: 6-size.c
  * Auth:Venancio Velez
  */
 #include <stdio.h>
 /*
- *main - Prints various types based on
+ * main - Prints various types based on
  *	the computer it is compiles and run on..
  *
  * Return: Always 0.
@@ -13,9 +13,9 @@ int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof (long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof (long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof (float));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
