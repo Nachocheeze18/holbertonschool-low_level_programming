a8ab83e916dba65d34e7f8eb66341589e3423fab
Nachocheeze18 <5581@holbertonstudents.com>
2022-09-19 17:55:04 -0700

    print the alphabet without q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,12 +12,10 @@ int main(void)
 	while (c <= 'z')
 	{
 		if (c != 'q' && c != 'e')
-		{
-																putchar(c);
-															}
-		c++;
+		{			
+				putchar(c);										}
+ 		c++;
 	}
 	putchar('\n');
 	return (0);
-
 }
