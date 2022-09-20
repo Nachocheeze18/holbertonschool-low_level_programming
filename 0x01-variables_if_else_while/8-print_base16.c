0201631d02cc64bc2fec6582b83a604b5db371eb
Nachocheeze18 <5581@holbertonstudents.com>
2022-09-20 10:28:04 -0700

    script to print the base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print all the numbers of 
+ * base 16 in reverse.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	i = 'a';
+	while (i <= 'f')
+	{
+		putchar(i)
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
