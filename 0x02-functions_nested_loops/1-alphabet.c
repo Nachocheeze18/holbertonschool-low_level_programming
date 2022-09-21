1563fba57becb6bd687c8ef32fceca01ff800457
Nachocheeze18 <5581@holbertonstudents.com>
2022-09-20 19:49:01 -0700

    script to print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
-#include "holberton.h"
+#include <main.h>
 
 /**
  * main - print alphabet in lowercase.
  *
  * Return:0 on success
  */
-int main(void)
+int print_alphabet(void)
 {
 	int n;
 
-	print_alphabet(n = 'a'; n <= 'z'; n++)
+	for (n = 'a'; n <= 'z'; n++)
 	{
 		putchar(n);
 	}
