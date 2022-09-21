e1255ef024fec8683113921c2f3a99d36106d0ac
Nachocheeze18 <5581@holbertonstudents.com>
2022-09-20 19:27:44 -0700

    script to print _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase.
+ *
+ * Return:0 on success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	return (0);
+}
