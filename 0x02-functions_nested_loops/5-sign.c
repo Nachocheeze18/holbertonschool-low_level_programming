8a558a3bf8540319fdcdffa62c67e54b7bf9d8aa
Nachocheeze18 <5581@holbertonstudents.com>
2022-09-21 12:06:53 -0700

    script to return to 1 if n>0 0 if n=0 -1 if n<0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints + if n is greater than 0 
+ * prints 0 if n is zero prints - if n is less than zero
+ *
+ * @n:carrier variable
+ *
+ * Return 1 if n>0, 0 if n==0, -1 if n<0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar(0);
+	return (0);
+}	
