20502207e67ca3b2c9fe4392dd02cb7625c3da53
Nachocheeze18 <nachovelez490@gmail.com>
2023-08-23 22:18:22 -0500

    linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -8,19 +8,18 @@
  *
  * Return: Index of the found value, or -1 if not found.
  */
-int linear_search(int *array, size_t size, int value) {
-    size_t i;
-    
+int linear_search(int *array, size_t size, int value)
+{
     if (array == NULL) {
         return -1;
     }
-    
-    for (i = 0; i < size; ++i) {
-        printf("Comparing with array[%lu]: %d\n", (unsigned long)i, array[i]);
+
+    for (size_t i = 0; i < size; ++i) {
+        printf("Value checked array[%zu] = [%d]\n", i, array[i]);
         if (array[i] == value) {
             return (int)i;
         }
     }
-    
+
     return -1;
 }
