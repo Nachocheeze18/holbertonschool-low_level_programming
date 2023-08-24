9547bf7bad58f36f8faa8b570fbe45e2ad9faa11
Nachocheeze18 <nachovelez490@gmail.com>
2023-08-23 22:22:39 -0500

    linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,11 +10,13 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+    size_t i; // Declare the loop variable outside the loop
+
     if (array == NULL) {
         return -1;
     }
 
-    for (size_t i = 0; i < size; i++) {
+    for (i = 0; i < size; i++) {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
         if (array[i] == value) {
