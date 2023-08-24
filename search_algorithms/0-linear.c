c6c262e375a4fdf89e3e62f8f01373e4878f2f0c
Nachocheeze18 <nachovelez490@gmail.com>
2023-08-23 22:21:21 -0500

    linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -13,15 +13,14 @@ int linear_search(int *array, size_t size, int value)
     if (array == NULL) {
         return -1;
     }
-    
-    unsigned long i; // Use unsigned long for iteration and printing
-    
-    for (i = 0; i < size; ++i) {
+
+    for (size_t i = 0; i < size; i++) {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
         if (array[i] == value) {
-            return (int)i;
+            return i;
         }
     }
-    
+
     return -1;
 }
